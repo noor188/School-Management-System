a9a3abca9e2319c489b9467c2c5b61bc9e6c6cb5
noor188 <noormmlk@gmail.com>
2025-02-09 15:22:43 -0700

    Model layer

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -14,7 +15,43 @@ import java.util.Set;
  * information and a mapping of 'courses' that indicate an inverse or referencing side
  * of the relationship. Implement Lombok annotations to eliminate boilerplate code.
  */
-
+@Entity
+@Table(name = "course")
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString(exclude = "students")
 public class Course {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CourseID")
+    private int id;
+
+    @Column(name = "name", length = 50, nullable = false)
+    @NonNull
+    private String name;
+
+    @Column(name = "instructor", length = 50, nullable = false)
+    @NonNull
+    private String instructor;
+
+    @ManyToMany(mappedBy = "courses", cascade = { CascadeType.DETACH, CascadeType.REMOVE, CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.EAGER)
+    private Set<Student> students = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Course course = (Course) o;
+        return this.id == course.id;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
 }
